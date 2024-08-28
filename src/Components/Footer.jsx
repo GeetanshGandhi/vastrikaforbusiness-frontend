d4a94253a9830ed26debcfeae7a58c58f74fc157
akv-dev339 <akshayvermaakv07@gmail.com>
2024-08-28 23:32:47 +0530

    Footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+    <div className="fl">
+        {/* //<img src={footer_logo} alt="" /></div>  */}
+        <p>VASTRIKA</p>
+        <ul className="footer-links">
+            <li>Products</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+        </div>
+        <div className="footer-copyright">
+            <hr />
+            <p>Copyright @ 2024 - All rights Reserved</p>
+                          
+        </div>
+</div>
+  )
+}
+
+export default Footer
